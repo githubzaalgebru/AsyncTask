7768b679d5447677b95d524bc65ec2fabd082fc9
office <bla@bla.com>
2022-02-02 18:21:09 +0100

    Zadnja verzija

--- app/src/main/java/com/algebra/asynctask/MainActivity.kt
+++ app/src/main/java/com/algebra/asynctask/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.algebra.asynctask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -42,4 +45,22 @@ class MainActivity : AppCompatActivity( ) {
             .makeText( this, "TEST", Toast.LENGTH_SHORT )
             .show( )
     }
+
+    override fun onCreateOptionsMenu( menu : Menu? ): Boolean {
+        menuInflater.inflate( R.menu.glavni, menu )
+        return super.onCreateOptionsMenu( menu )
+    }
+
+    override fun onOptionsItemSelected( item : MenuItem ): Boolean {
+        val itemId = item.itemId
+        when( itemId ) {
+            R.id.web -> {
+                startActivity( Intent( this, SecondActivity::class.java ) )
+            }
+            R.id.link -> { }
+            R.id.toast -> { Toast.makeText( this, "Stisnuto je TOAST", Toast.LENGTH_SHORT ).show( ) }
+        }
+        return super.onOptionsItemSelected( item )
+    }
+
 }
